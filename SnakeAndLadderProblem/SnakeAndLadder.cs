cf1a62fd4838038d497202e8f3e216741004d707
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-03 15:04:37 +0530

    [MaheshKangude] ADDED: SinglePlayer At Start Position

--- SnakeAndLadderProblem/SnakeAndLadder.cs
+++ SnakeAndLadderProblem/SnakeAndLadder.cs
@@ -4,9 +4,24 @@ namespace SnakeAndLadderProblem
 {
     class SnakeAndLadder
     {
+        private const int StartPosition = 0;
+        private int PlayerPostion;
+
+        public SnakeAndLadder()
+        {
+            PlayerPostion = StartPosition;
+        }
+
+        public void ShowPosition()
+        {
+            Console.WriteLine("Player position: "+PlayerPostion);
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake And Ladder Game");
+            SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
+            snakeAndLadder.ShowPosition();
+
         }
     }
 }
