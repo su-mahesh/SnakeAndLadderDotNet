12d6602e16c1c7d9958724f9ff529f8d7262527f
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-03 16:39:44 +0530

    [Mahesh Kangude] ADDED: fuctionality to play on given options

--- SnakeAndLadderProblem/SnakeAndLadder.cs
+++ SnakeAndLadderProblem/SnakeAndLadder.cs
@@ -7,6 +7,9 @@ namespace SnakeAndLadderProblem
         private const int StartPosition = 0;
         private int PlayerPostion;
 
+        public enum PlayOption {
+            No_Play, Ladder, Snake
+        }
         public SnakeAndLadder()
         {
             PlayerPostion = StartPosition;
@@ -21,17 +24,35 @@ namespace SnakeAndLadderProblem
             return new Random().Next(1, 7);
         }
 
-        public void RollTheDice()
-        {
-            Console.WriteLine("Dice rolled: " +GetDiceNumber());
+        public PlayOption GetPlayOption() {
+           
+            return (PlayOption)new Random().Next(0, 3);            
         }
 
+        public void MakeMove() {
+            int NextPosition = GetDiceNumber();
+            Console.WriteLine("Dice rolled: "+ NextPosition);
+            switch (GetPlayOption()) {
+                case PlayOption.Ladder:
+                    Console.WriteLine("Play option: Ladder");
+                    PlayerPostion += NextPosition;
+                    break;
+                case PlayOption.Snake:
+                    Console.WriteLine("Play option: Snake");
+                    PlayerPostion -= NextPosition;
+                    break;
+                default:
+                    Console.WriteLine("Play option: NO PLAY");
+                    break;
+            }
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake And Ladder Game");
             SnakeAndLadder snakeAndLadder = new SnakeAndLadder();
             snakeAndLadder.ShowPosition();
-            snakeAndLadder.RollTheDice();
+            snakeAndLadder.MakeMove();
+            snakeAndLadder.ShowPosition();
         }
     }
 }
