6e4b11322b4722c88eebb98af9b8ddecb8c0efc1
Mahesh Kangude <Mahesh Kangude@DESKTOP-DF3BBMR>
2021-02-03 14:50:50 +0530

    [MaheshK] ADDED: main class with welcome message

---
+++ SnakeAndLadderProblem/SnakeAndLadder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeAndLadderProblem
+{
+    class SnakeAndLadder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake And Ladder Game");
+        }
+    }
+}
